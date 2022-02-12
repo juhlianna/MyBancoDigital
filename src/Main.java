e38df9f55534ac7712e34f943ace56416df51fa7
Juliana Pessoa Panini <86628688+juhlianna@users.noreply.github.com>
2022-02-12 14:51:17 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Cliente venilton = new Cliente();
-        venilton.setNome("Juliana");
+        Cliente juliana = new Cliente();
+        juliana.setNome("Juliana");
 
-        Conta cc = new ContaCorrente(venilton);
-        Conta poupanca = new ContaPoupanca(venilton);
+        Conta cc = new ContaCorrente(juliana);
+        Conta poupanca = new ContaPoupanca(juliana);
 
         cc.depositar(100);
         cc.transferir(100, poupanca);
